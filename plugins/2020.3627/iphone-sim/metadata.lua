b9d585232a9641cdb9885b73d1504b69504c5029
Ansh3ll <ansh3ll@protonmail.com>
2022-05-07 13:27:15 +0300

    Add iphone-sim libs

---
+++ plugins/2020.3627/iphone-sim/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+    plugin =
+    {
+        format = 'staticLibrary',
+        staticLibs = { 'plugin_movie', 'ogg', 'theoradec', 'vorbis', },
+        frameworks = {},
+        frameworksOptional = {},
+    },
+}
+
+return metadata
