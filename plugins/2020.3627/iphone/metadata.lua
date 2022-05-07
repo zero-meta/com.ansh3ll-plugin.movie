7a139509e326c07a4ae5c5196f1fb6138c9e1d4b
Ansh3ll <ansh3ll@protonmail.com>
2022-05-07 12:46:25 +0300

    Add libs

---
+++ plugins/2020.3627/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+    plugin =
+    {
+        format = 'sharedLibrary',
+        staticLibs = { 'plugin.movie', 'ogg', 'theoradec', 'vorbis', },
+        frameworks = {},
+        frameworksOptional = {},
+    },
+}
+
+return metadata
